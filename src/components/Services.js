639ea6b7e0f4c24844920efab41dc9532975f2ac
Emanuel Corrêa <tenseioficial@gmail.com>
2024-10-22 17:43:51 -0300

    animação na sessão de serviços

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 const Services = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
-
+    const [isColapse, setIsColapsed] = useState(false);
+    const [showContent, setShowContent] = useState(true);
+    const sectionRef = useRef(null);
+    const semicircleRef = useRef(null);
     const services = [
         {
             title: "BeeSpot",
@@ -38,23 +41,56 @@ const Services = () => {
         beforeChange: (current, next) => setCurrentSlide(next),
     };
 
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    setTimeout(() => {
+                        setIsColapsed(true);
+                        setTimeout(() => setShowContent(true), 1000);
+                    }, 500);
+                } else {
+                    setIsColapsed(false);
+                    setShowContent(false);
+                }
+            },
+            { threshold: 0.3 } 
+        );
+
+        if (sectionRef.current) {
+            observer.observe(sectionRef.current);
+        }
+
+        return () => {
+            if (sectionRef.current) {
+                observer.unobserve(sectionRef.current);
+            }
+        };
+    }, []);
+
+    
     return (
-        <div className="flex h-screen">
-            <div className="w-1/2 relative overflow-hidden">
-                <div 
-                    className="absolute inset-y-0 left-0 rounded-r-full h-full w-[200%] transform -translate-x-1/2 bg-cover bg-center transition-all duration-500 ease-in-out"
-                    style={{ 
-                        backgroundImage: `url(${services[currentSlide].image})`,
-                    }}
-                >
-                    <div className="absolute inset-0 bg-black opacity-70 rounded-r-full"></div>
-                </div>
+        <div ref={sectionRef} className="flex h-screen relative overflow-hidden">
+            <div 
+                ref={semicircleRef}
+                className={`absolute inset-0 bg-cover bg-center transition-all duration-1000 ease-in-out ${
+                    isColapse 
+                        ? 'w-[75%] -translate-x-1/3 rounded-r-full' 
+                        : 'w-full'
+                }`}
+                style={{ 
+                    backgroundImage: `url(${services[currentSlide].image})`,
+                }}
+            >
+                <div className={`absolute inset-0 bg-black opacity-70 transition-all duration-1000 ${isColapse ? 'rounded-r-full' : ''}`}></div>
+            </div>
+            <div className={`w-1/2 relative overflow-hidden transition-all duration-1000 ${isColapse ? 'opacity-100' : 'opacity-0'}`}>
                 <Slider {...settings} className="relative z-10 h-full pt-40">
                     {services.map((service, index) => (
                         <div key={index} className="h-full relative flex items-center justify-center">
                             <a 
                                 href={service.link} 
-                                className="flex flex-col items-center justify-center h-full p-5 text-bit-bee-yellow cursor-pointer pt-40" 
+                                className={`flex flex-col items-center justify-center h-full p-5 text-bit-bee-yellow cursor-pointer pt-40 transition-opacity duration-500 ${showContent ? 'opacity-100' : 'opacity-0'}`}
                             >
                                 <h3 className="text-4xl font-bold text-center">{service.title}</h3>
                                 <p className="text-lg text-center mb-4">{service.description}</p>
@@ -63,7 +99,7 @@ const Services = () => {
                     ))} 
                 </Slider>
             </div>
-            <div className="w-1/2 flex items-center justify-center p-10">
+            <div className={`w-1/2 flex items-center justify-center p-10 transition-all duration-1000 ${isColapse? 'opacity-100' : 'opacity-0'}`}>
                 <div>
                     <h2 className="text-4xl font-bold mb-4 text-bit-bee-yellow">Nossos Serviços</h2>
                     <p className="text-lg text-black">
